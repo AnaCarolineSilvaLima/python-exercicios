b17ff6d9b8114c460482615c28cff4572c78d05f
AnaCarolineSilvaLima <anacarolinelima1212@gmail.com>
2024-05-07 11:24:16 -0300

    atualização

---
+++ entrada_saida/exercicio_4.py
@@ -0,0 +1,6 @@
+#Entrada de Dados
+nota1 = float(input("Insira a nota da primeira prova: "))
+nota2 = float(input("Insira a nota da segunda prova: "))
+nota3 = float(input("Insira a nota da terceira prova: "))
+média = (nota1+nota2+nota3/3)
+print(f"A médias das notas é: {média}")
